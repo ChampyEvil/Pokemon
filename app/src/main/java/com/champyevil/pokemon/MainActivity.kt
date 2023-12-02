ccd9092dc146b0a0b5ed260043c2ee1f465218e6
Champ <champ@odds.team>
2023-12-02 14:18:24 +0700

    Implement on click pokeball

--- app/src/main/java/com/champyevil/pokemon/MainActivity.kt
+++ app/src/main/java/com/champyevil/pokemon/MainActivity.kt
@@ -2,6 +2,7 @@ package com.champyevil.pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.champyevil.pokemon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.pokeball.setOnClickListener {
+            Log.d(
+                MainActivity::class.java.simpleName, "on click"
+            )
+        }
     }
 }
