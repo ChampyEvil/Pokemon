9b665b881b96eb4ca35f6a721dee7c89a49584b9
Champ <champ@odds.team>
2023-12-03 14:40:27 +0700

    Create interface for pokemon api

--- app/src/main/java/com/champyevil/pokemon/api/PokemonApi.kt
+++ app/src/main/java/com/champyevil/pokemon/api/PokemonApi.kt
@@ -1,4 +1,11 @@
 package com.champyevil.pokemon.api
 
+import com.champyevil.pokemon.model.Pokemon
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface PokemonApi {
+
+    @GET("pokemons")
+    fun getPokemonList(): Response<List<Pokemon>>
 }
